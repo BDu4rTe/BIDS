fea52e6c72bf52e504854a2b0e8d2c95a392ebdd
BDu4rTe <brianschierduarte@outlook.com>
2022-06-20 13:04:13 -0300

    criacao do projeto e config da pasta theme

---
+++ src/theme/colors.js
@@ -0,0 +1,21 @@
+export default {
+    primary: "#72BAAC",
+    primaryShadow: "#568178",
+
+    success: "#25D366",
+    successShadow: "#1EAE54",
+    warning: "#FCC936",
+    warningShadow: "#C99603",
+    danger: "#EA4335",
+    dangerShadow: "#B92013",
+
+    card: "#E5E5E5",
+    cardShadow: "#979797",
+
+    text: "#3D3D3D",
+    textAltLight: "#ffffff",
+
+    shadow: "#e5e5e5",
+
+    background: "#FFFFFF",
+}
